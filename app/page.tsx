7fb7819e60263619294f74128f77efd12a65f535
bogdan shulika <bsshul@gmail.com>
2024-06-22 23:37:35 +0200

    Image placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getAllProducts } from "./actions";
 import { Product } from "./lib/types/product.interfase";
+import Image from "next/image";
 
 export default async function Home() {
   const products: Product[] = await getAllProducts();
@@ -11,6 +12,27 @@ export default async function Home() {
           <div key={product.id} className="border-2 rounded p-2 mb-2">
             <p className="text-lg font-semibold">{product.title}</p>
             <p className="mt-2">Price: {product.price}</p>
+            {product.image ? (
+              <Image
+                src={product.image}
+                alt={product.title}
+                width={100}
+                height={100}
+                className="w-full object-contain bg-gray-200 rounded"
+                priority
+                quality={100}
+              />
+            ) : (
+              <Image
+                src={"/prod.svg"}
+                alt={product.title}
+                width={200}
+                height={200}
+                className="w-full object-contain bg-gray-200 rounded"
+                priority
+                quality={100}
+              />
+            )}
           </div>
         ))}
       </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "json-server-vercel-shop.vercel.app",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
